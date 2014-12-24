b725f818f85265bfa488a3337a5128ff047ca20b
lhepler <lhepler@pacificbiosciences.com>
2014-12-24 11:10:32 -0800

    zlib shenanigans brought on by recent changes bug 25263
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/lib/cpp/": change = 143897]

--- common.mk
+++ common.mk
@@ -21,12 +21,29 @@ PREBUILT ?= ../../../../prebuilt.out
 # this is a magic string from our build system
 OS_STRING2 ?= centos-5
 
-HDF5_ROOT := $(shell cd $(PREBUILT)/hdf5/hdf5-1.8.12/$(OS_STRING2)/artifact 2>/dev/null && pwd || echo -n notfound)
+# handle HDF5_ROOT
+ifeq ($(origin HDF5_ROOT), undefined)
+	HDF5_ROOT := $(shell cd $(PREBUILT)/hdf5/hdf5-1.8.12/$(OS_STRING2)/artifact 2>/dev/null && pwd || echo -n notfound)
+else
+	HDF5_ROOT := $(shell cd $(HDF5_ROOT) 2>/dev/null && pwd || echo -n notfound)
+endif
+
 ifeq ($(HDF5_ROOT), notfound)
 	HDF5_ROOT := $(shell cd $(THIRD_PARTY)/hdf5 2>/dev/null && pwd || echo -n notfound)
 	ifeq ($(HDF5_ROOT), notfound)
 		$(error please set HDF5_ROOT to a built source tree of hdf5.)
 	endif
+endif
+
+# handle ZLIB_ROOT
+ifeq ($(origin ZLIB_ROOT), undefined)
+	ZLIB_ROOT := $(shell cd $(PREBUILT)/zlib/zlib-1.2.5/$(OS_STRING2) 2>/dev/null && pwd || echo -n notfound)
+else
+	ZLIB_ROOT := $(shell cd $(ZLIB_ROOT) 2>/dev/null && pwd || echo -n notfound)
+endif
+
+ifeq ($(ZLIB_ROOT), notfound)
+	# we don't need a backup ZLIB_ROOT here, because almost everybody has one in their paths
 endif
 
 # magic for non-verbose builds
