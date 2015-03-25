0a3a478c6daa7ea6568887e58d452a989fd7f90d
lhepler <lhepler@pacificbiosciences.com>
2015-03-25 15:19:58 -0800

    Silence lsb_release not found error, tis annoying and verbose    [bug 25263]    #review-147823
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/lib/cpp/": change = 147827]

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
 SHELL          = bash
-G_BUILDOS_CMD := bash -c 'set -e; set -o pipefail; id=$$(lsb_release -si | tr "[:upper:]" "[:lower:]"); rel=$$(lsb_release -sr); case $$id in ubuntu) printf "$$id-%04d\n" $${rel/./};; centos) echo "$$id-$${rel%%.*}";; *) echo "$$id-$$rel";; esac'
+G_BUILDOS_CMD := bash -c 'set -e; set -o pipefail; id=$$(lsb_release -si | tr "[:upper:]" "[:lower:]"); rel=$$(lsb_release -sr); case $$id in ubuntu) printf "$$id-%04d\n" $${rel/./};; centos) echo "$$id-$${rel%%.*}";; *) echo "$$id-$$rel";; esac' 2>/dev/null
 OS_STRING     ?= $(shell $(G_BUILDOS_CMD))
 
 #
