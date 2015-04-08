66beab1b1f7f46438e2287bc109941ee8b62b725
Herb Maeder <hmaeder@pacificbiosciences.com>
2015-04-08 15:06:48 -0800

    modbuild: ml: bug 26259: makefiles mods to support modular build
    Separate checkin of the existing makefiles and cmake files that need
    to be modified in order to support the new modular build system.
    
    For the most part, the modifications to the makefiles involve
    replacing some hardwired text with variables that can be overridden
    on the make command line.  
    
    And similarly, in the CMakeLists.txt files, command line options were
    added so that they could be used in conditionals and overridden on
    the command line.
    
    The intent is that there should be no fucntional differences
    introduced by this change (i.e. fully compatible with the previous
    version).
    
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/lib/cpp/": change = 148718]

--- common.mk
+++ common.mk
@@ -2,51 +2,56 @@ SHELL          = bash
 G_BUILDOS_CMD := bash -c 'set -e; set -o pipefail; id=$$(lsb_release -si | tr "[:upper:]" "[:lower:]"); rel=$$(lsb_release -sr); case $$id in ubuntu) printf "$$id-%04d\n" $${rel/./};; centos) echo "$$id-$${rel%%.*}";; *) echo "$$id-$$rel";; esac' 2>/dev/null
 OS_STRING     ?= $(shell $(G_BUILDOS_CMD))
 
-#
-# Definitions common to all make files for library code.
-# All paths are relative from inside the subdirectories, not this file
-#
+ifneq ($(COMMON_NO_THIRD_PARTY_REQD),true)
+    #
+    # Definitions common to all make files for library code.
+    # All paths are relative from inside the subdirectories, not this file
+    #
 
-THIRD_PARTY_PREFIX ?= ../..
+    THIRD_PARTY_PREFIX ?= ../..
 
-# git layout vs p4 layout automagic
-THIRD_PARTY ?= $(shell cd $(abspath $(THIRD_PARTY_PREFIX)/third-party) 2>/dev/null && pwd || echo -n notfound)
-ifeq ($(THIRD_PARTY), notfound)
+    # git layout vs p4 layout automagic
+    THIRD_PARTY ?= $(shell cd $(abspath $(THIRD_PARTY_PREFIX)/third-party) 2>/dev/null && pwd || echo -n notfound)
+    ifeq ($(THIRD_PARTY), notfound)
 	THIRD_PARTY := $(shell cd $(abspath $(THIRD_PARTY_PREFIX)/../third-party/cpp) 2>/dev/null && pwd || echo -n notfound)
-	ifeq ($(THIRD_PARTY), notfound)
-		$(error cannot find third-party libraries!)
-	endif
-endif
+    endif
 
-PREBUILT ?= $(realpath ../../../../prebuilt.out)
+    PREBUILT ?= $(realpath ../../../../prebuilt.out)
 
-# handle HDF5_INC HDF5_LIB
-ifeq ($(origin HDF5_INC), undefined)
+    # handle HDF5_INC HDF5_LIB
+    ifeq ($(origin HDF5_INC), undefined)
 	HDF5_INC := $(shell cd $(PREBUILT)/hdf5/hdf5-1.8.12/$(OS_STRING)/include 2>/dev/null && pwd || echo -n notfound)
-else
+    else
 	HDF5_INC := $(shell cd $(HDF5_INC) 2>/dev/null && pwd || echo -n notfound)
-endif
-ifeq ($(HDF5_INC), notfound)
+    endif
+    ifeq ($(HDF5_INC), notfound)
+	ifeq ($(THIRD_PARTY), notfound)
+                $(error cannot find third-party libraries!)
+	endif
 	HDF5_INC := $(shell cd $(THIRD_PARTY)/hdf5/include 2>/dev/null && pwd || echo -n notfound)
-endif
-ifeq ($(origin HDF5_LIB), undefined)
+    endif
+    ifeq ($(origin HDF5_LIB), undefined)
 	HDF5_LIB := $(shell cd $(PREBUILT)/hdf5/hdf5-1.8.12/$(OS_STRING)/lib 2>/dev/null && pwd || echo -n notfound)
-else
+    else
 	HDF5_LIB := $(shell cd $(HDF5_LIB) 2>/dev/null && pwd || echo -n notfound)
-endif
-ifeq ($(HDF5_LIB), notfound)
+    endif
+    ifeq ($(HDF5_LIB), notfound)
+	ifeq ($(THIRD_PARTY), notfound)
+                $(error cannot find third-party libraries!)
+	endif
 	HDF5_LIB := $(shell cd $(THIRD_PARTY)/hdf5/lib 2>/dev/null && pwd || echo -n notfound)
-endif
+    endif
 
-# handle ZLIB_ROOT
-ifeq ($(origin ZLIB_ROOT), undefined)
+    # handle ZLIB_ROOT
+    ifeq ($(origin ZLIB_ROOT), undefined)
 	ZLIB_ROOT := $(shell cd $(PREBUILT)/zlib/zlib-1.2.5/$(OS_STRING) 2>/dev/null && pwd || echo -n notfound)
-else
+    else
 	ZLIB_ROOT := $(shell cd $(ZLIB_ROOT) 2>/dev/null && pwd || echo -n notfound)
-endif
+    endif
 
-ifeq ($(ZLIB_ROOT), notfound)
+    ifeq ($(ZLIB_ROOT), notfound)
 	# we don't need a backup ZLIB_ROOT here, because almost everybody has one in their paths
+    endif
 endif
 
 # handle PBBAM
