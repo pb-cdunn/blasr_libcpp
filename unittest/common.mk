9e1818477fb7498a9c181c643c92e17de173a6b9
Yuan Li <yli@pacificbiosciences.com>
2015-04-23 13:58:03 -0800

    bug 26058 Link to libhts.a built with pbbam when it's available in order to prevent runtime errors caused by dynamically linking to an incompatible libhts.
    [git-p4: depot-paths = "//depot/software/smrtanalysis/bioinformatics/lib/cpp/": change = 149557]

--- unittest/common.mk
+++ unittest/common.mk
@@ -7,7 +7,7 @@ SHELL=bash
 # All paths are relative from inside the subdirectories, not this file
 #
 
-PBINCROOT 	   ?= ../..
+PBINCROOT 	   ?= $(realpath ../..)
 PREBUILT    	   ?= $(realpath ../../../../../prebuilt.out)
 THIRD_PARTY_PREFIX ?= ../../..
 
@@ -18,6 +18,47 @@ GTEST_ROOT := $(THIRD_PARTY)/gtest/fused-src
 GTEST_SRC  := $(GTEST_ROOT)/gtest/gtest-all.cc \
               $(GTEST_ROOT)/gtest/gtest_main.cc
 
+# INCLUDE DIRS
+INCDIRS    = -I$(PBINCROOT)/alignment \
+             -I$(PBINCROOT)/hdf \
+             -I$(PBINCROOT)/pbdata \
+			 -I$(PBINCROOT)/unittest \
+             -I$(GTEST_ROOT) \
+             -I$(HDF5_INC)
+
+# LIB DIRS
+LIBDIRS    = -L$(PBINCROOT)/alignment \
+             -L$(PBINCROOT)/hdf \
+             -L$(PBINCROOT)/pbdata \
+             -L$(HDF5_LIB) 
+
+LDFLAGS1 := -lblasr -lpbihdf -lpbdata 
+# The order of -l{lib} matters
+
+ifeq ($(origin nopbbam), undefined)
+	INCDIRS += -I$(PBBAM)/include -I$(PBBAM)/third-party/htslib
+	LIBDIRS += -L$(PBBAM)/lib -L$(PBBAM)/third-party/htslib
+	LDFLAGS1 += -lpbbam
+# Use libhts.a built with pbbam
+ifneq ($(wildcard "$(PBBAM)/third-party/htslib/libhts.a"), "")
+	LDFLAGS1 += $(PBBAM)/third-party/htslib/libhts.a 
+else
+    LDFLAGS1 += lhts
+endif
+endif
+
+ifneq ($(ZLIB_ROOT), notfound)
+	INCDIRS += -I$(ZLIB_ROOT)/include
+	LIBDIRS += -L$(ZLIB_ROOT)/lib
+endif
+
+ifneq ($(wildcard "$(HDF5_LIB)/libhdf5_cpp.a"),"")
+    LDFLAGS   := $(LDFLAGS1) $(HDF5_LIB)/libhdf5_cpp.a $(HDF5_LIB)/libhdf5.a -lpthread -lz -ldl
+else
+    LDFLAGS   := $(LDFLAGS1) -lhdf5_cpp -lhdf5 -lpthread -lz -ldl
+endif
+
+
 CXX := g++
 CXXOPTS := -std=c++11 -Wno-div-by-zero 
 
@@ -26,3 +67,5 @@ all : CXXFLAGS ?= -O3
 debug : CXXFLAGS ?= -g -ggdb -fno-inline
 
 profile : CXXFLAGS ?= -Os -pg
+
+
