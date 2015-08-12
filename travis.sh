52a8cde306445b59752e8577e240dee2deb41eff
Christopher Dunn <cdunn@pacificbiosciences.com>
2015-08-12 00:16:16 -0700

    try fewer jobs

--- travis.sh
+++ travis.sh
@@ -3,5 +3,5 @@ set -ex
 ( cd pbdata; NOPBBAM=1 ../configure.py; )
 ( cd hdf; NOPBBAM=1 ../configure.py; )
 ( cd alignment; NOPBBAM=1 ../configure.py; )
-make -j all
-# make -j gtest
+make -j4 all
+# make -j4 gtest
