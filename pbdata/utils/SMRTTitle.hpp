c976416a1afce8bdf726159c3ee096fc2d7643c0
Yuan Li <yli@pacificbiosciences.com>
2015-09-26 21:35:08 -0700

    SMRTTitle: Added public functions to access member variables

--- pbdata/utils/SMRTTitle.hpp
+++ pbdata/utils/SMRTTitle.hpp
@@ -20,5 +20,19 @@ public:
     /// \returns smrt title movie/zmw/s_e, if input read is a smrt title;
     /// otherwise, return an empty string.
     std::string ToString();
+
+public:
+    inline std::string MovieName(void) const;
+    inline UInt HoleNumber(void) const;
+    inline DNALength Start(void) const;
+    inline DNALength End(void) const;
+    inline operator bool(void) const;
 };
+
+inline std::string SMRTTitle::MovieName(void) const {return movieName;}
+inline UInt SMRTTitle::HoleNumber(void) const {return holeNumber;}
+inline DNALength SMRTTitle::Start(void) const {return start;}
+inline DNALength SMRTTitle::End(void) const {return end;}
+inline SMRTTitle::operator bool(void) const {return isSMRTTitle;}
+
 #endif
