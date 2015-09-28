b0076f39aa6e27c6ea083a79a40a63241960a594
Yuan Li <yli@pacificbiosciences.com>
2015-09-28 01:08:10 -0700

    ReadInterval: added constructor to convert RegionAnnotation to ReadInterval.

--- pbdata/reads/ReadInterval.hpp
+++ pbdata/reads/ReadInterval.hpp
@@ -1,18 +1,29 @@
 #ifndef _BLASR_READ_INTERVAL_HPP_
 #define _BLASR_READ_INTERVAL_HPP_
 
+#include "RegionAnnotation.hpp"
+
+class RegionAnnotation;
+
 class ReadInterval {
 public:
     int start;
     int end;
     int score;
+
     ReadInterval(int s, int e, int sc=0) : start(s), end(e), score(sc) {};
+
     ReadInterval& operator=(const ReadInterval &rhs) {
         start = rhs.start;
         end   = rhs.end;
         score = rhs.score;
         return *this;
     }
+
+    ReadInterval(const RegionAnnotation & ra)
+    : start(ra.GetStart())
+    , end(ra.GetEnd())
+    , score(ra.GetScore()) {}
 };
 
 #endif
